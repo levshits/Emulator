7383ee96a8392cfa625e767eff52aa533b77c543
levshits <levshits.l@gmail.com>
2016-05-04 21:46:10 +0300

    Popup was added

--- Emulator/Logic/ApplyFilterCommand.cs
+++ Emulator/Logic/ApplyFilterCommand.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Emulator.Common.Interfaces;
 using Emulator.Common.Models;
+using Emulator.View;
+using GalaSoft.MvvmLight.Command;
+using MaterialDesignThemes.Wpf;
 
 namespace Emulator.Logic
 {
@@ -13,9 +18,20 @@ namespace Emulator.Logic
             return true;
         }
 
-        public void Execute(object parameter)
+        public async void Execute(object parameter)
         {
-            DataProvider.ApplyFilter((FilterModel) parameter);
+            var view = new FilterDialog
+            {
+                DataContext = new {Command = new RelayCommand( async () =>
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(3));
+                    DialogHost.CloseDialogCommand.Execute(null, null);
+                    DataProvider.ApplyFilter((FilterModel)parameter);
+                })}
+            };
+            await DialogHost.Show(view, "RootDialog");
+
+
         }
 
         public event EventHandler CanExecuteChanged
